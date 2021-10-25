59058d6a5e36675d66219904ddcdd1d86e293be9
Moki <mokiong1427@gmail.com>
2021-10-25 14:47:32 +0800

    Create dummy data holder

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
 import { useRef } from 'react';
 
 function HomePage() {
-    const ref = useRef();
+    const emailInput = useRef();
+    const feedBackInput = useRef();
+
+    function submitFormHandler(event) {
+        event.preventDefault();
+
+        const enteredEmail = emailInput.current.value;
+        const enteredFeedBack = feedBackInput.current.value;
+
+        const reqBody = { email: enteredEmail, text: enteredFeedBack };
+
+        fetch('/api/feedback', {
+            method: 'POST',
+            body: JSON.stringify(reqBody),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+    }
 
     return (
         <div>
             <h1>The Home Page</h1>
 
-            <form>
+            <form onSubmit={submitFormHandler}>
                 <div>
                     <label htmlFor="email">Your email address</label>
-                    <input type="email" id="email" />
+                    <input type="email" id="email" ref={emailInput} />
                 </div>
                 <div>
                     <label htmlFor="feedback">Your Feedback</label>
-                    <textarea type="text" id="feedback" rows="5" />
+                    <textarea
+                        type="text"
+                        id="feedback"
+                        rows="5"
+                        ref={feedBackInput}
+                    />
                 </div>
                 <button>Send</button>
             </form>
