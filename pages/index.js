7a323064df98fe0b6ad03f5c187f2e36d3b62229
Moki <mokiong1427@gmail.com>
2021-10-25 13:21:31 +0800

    Create form

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
+import { useRef } from 'react';
+
 function HomePage() {
-  return (
-    <div>
-      <h1>The Home Page</h1>
-    </div>
-  );
+    const ref = useRef();
+
+    return (
+        <div>
+            <h1>The Home Page</h1>
+
+            <form>
+                <div>
+                    <label htmlFor="email">Your email address</label>
+                    <input type="email" id="email" />
+                </div>
+                <div>
+                    <label htmlFor="feedback">Your Feedback</label>
+                    <textarea type="text" id="feedback" rows="5" />
+                </div>
+                <button>Send</button>
+            </form>
+        </div>
+    );
 }
 
 export default HomePage;
